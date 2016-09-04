4a7d280a8dcd606d72a0c8d7242ab5bd5a49066e
Natalya Kot <natalyavaskot@gmail.com>
2016-09-04 17:44:52 +0300

    Анимировала появление формы

--- js/popup.js
+++ js/popup.js
@@ -1,5 +1,10 @@
 var open = document.querySelector(".open-form");
 var popup = document.querySelector(".modal-content");
+var form = popup.querySelector("form");
+var checkIn = popup.querySelector("[name=check-in]");
+var checkOut = popup.querySelector("[name=check-out]");
+var adults = popup.querySelector("[name=adults]");
+var children = popup.querySelector("[name=children]");
 
 open.addEventListener("click", function (event) {
   event.preventDefault();
@@ -12,4 +17,8 @@ window.addEventListener("keydown", function (event) {
       popup.classList.remove("modal-content-show");
     }
   }
+});
+
+ form.addEventListener("submit", function(event) {
+        event.preventDefault();
 });
