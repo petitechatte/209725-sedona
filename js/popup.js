55766b712e65c20a42295c0ec4c509bd8ac96056
Natalya Kot <natalyavaskot@gmail.com>
2016-09-04 19:37:44 +0300

    Добавила проверку формы на JS

--- js/popup.js
+++ js/popup.js
@@ -15,10 +15,17 @@ window.addEventListener("keydown", function (event) {
   if (event.keyCode === 27) {
     if (popup.classList.contains("modal-content-show")) {
       popup.classList.remove("modal-content-show");
+      popup.classList.add("modal-content-hide");
+      popup.classList.remove("modal-error");
     }
   }
 });
 
  form.addEventListener("submit", function(event) {
-        event.preventDefault();
+   if (!checkIn.value || !checkOut.value || !adults.value || !children.value) {
+      event.preventDefault();
+      popup.classList.remove("modal-error");
+      popup.offsetWidth = popup.offsetWidth;
+      popup.classList.add("modal-error");
+   }
 });
